a2fcc16a704397135789d6bdd56c18a8a6ed2dc8
maria <mariasharif.16@hotmail.com>
2024-07-09 14:48:28 +0100

    changes to css in userComponents

--- src/components/Users/UsersComponent.jsx
+++ src/components/Users/UsersComponent.jsx
@@ -8,19 +8,25 @@ const UsersComponent = ({users, setUsers, originalUserList}) => {
     const userList = users.map((user) => {
         return( 
             <section key={user.id} className="content-grid">
-                <p>{user.username}</p>
+                <p className="card">{user.username} </p>
             </section>
         )
     })
 
     return(
-        <div>
-            <SearchUsersComponent users={users} setUsers={setUsers} originalUserList={originalUserList}/>
-            <hr />
-             {userList}
-            <Link to = "/users/new">Add new user</Link>
+        <div className="users-container">
+             <div className="search-box">
+                <SearchUsersComponent users={users} setUsers={setUsers} originalUserList={originalUserList}/>
+                <hr />
+             </div >
+             <div className="content-grid">
+                {userList}
+                <Link to = "/users/new">Add new user</Link>
+            </div>
         </div>
     )
+
+
 }
 
 export default UsersComponent;
